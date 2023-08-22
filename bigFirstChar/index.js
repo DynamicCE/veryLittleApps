213dbb688b834089517adf66fdab0cb482d5c416
Adınız <erkanmurat0632@gmail.com>
2023-08-22 09:36:51 +0300

    ilk karakteri büyük yapan kod

---
+++ bigFirstChar/index.js
@@ -0,0 +1,11 @@
+let bigFirstChar = (word) => {
+let first = word[0];
+let rest = word.slice(1);
+
+return first+rest ;
+}
+
+console.log(bigFirstChar("ahmet"));
+
+
+// bu kod ilk karakteri büyük yapar.
