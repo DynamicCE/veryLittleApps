0ad8864679c73f53b8250b2e6f3ef9ddfccdd3a6
Adınız <erkanmurat0632@gmail.com>
2023-08-28 19:32:12 +0300

    dice game

---
+++ dice/index.js
@@ -0,0 +1,5 @@
+let randomNum = ()=>{
+   return Math.floor(Math.random()*6 +1);  // math.random , 0 ile 0.99 arasında bi sayı üretir 
+}
+// math.floor da virgüllü sayıyı bir küçük tam sayıya yuvarlar
+console.log(randomNum());
