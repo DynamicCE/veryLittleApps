d848022be8b864c27769a8d6a5282b8fdfd0ca3c
Adınız <erkanmurat0632@gmail.com>
2023-08-28 19:42:01 +0300

    promise

--- promises/index3.js
+++ promises/index3.js
@@ -1,3 +1,14 @@
 const myPromise = new Promise((resolve,reject)=>{
-    
+    let rand = Math.floor(Math.random() *9);
+    if(rand>5){
+        resolve("sayı 5 den büyük");
+    }else reject("sayı 5 den küçük");
+})
+
+myPromise
+.then((message)=>{
+    console.log(message);
+})
+.catch((error)=>{
+    console.log(error);
 })
