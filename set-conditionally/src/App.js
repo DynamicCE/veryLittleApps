09c0b424654531fed5fe40dabc10e26c9b5d8c93
Adınız <erkanmurat0632@gmail.com>
2023-10-06 13:31:44 +0300

    setting conditionally

--- set-conditionally/src/App.js
+++ set-conditionally/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const isTrue = true;
+  const deger = isTrue ? "evet doğru" : "hayır yanlış";
+  {
+    /* şimdi üst satırdaki ifade eğer isTrue doğru ise evet doğru stringini alacak,
+    yanlışsa da hayır yanlış  stringini alacak*/
+  }
+  return <div className="App">{`yanıtınız: ${deger}`}</div>;
 }
 
 export default App;
