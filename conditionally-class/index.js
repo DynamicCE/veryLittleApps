7672290c308a23ae156199ed563b5574dc5d32b6
Adınız <erkanmurat0632@gmail.com>
2023-11-02 17:48:39 +0300

    classNames library added

--- conditionally-class/index.js
+++ conditionally-class/index.js
@@ -4,10 +4,16 @@ import React from 'react';
 
 isTrue = true;
 
+
 <div className={isTrue ? 'if-true-here' : 'if-false-here'  }>content</div>
 
 {/* if we have already a clasName and we want to add another class we do this */}
-
 <div className={`main-class ${condition}`}></div>
 {/* for example : */}
 <div className={`base-class ${isTrue ? 'if-true-here': 'if-false-here'}`}></div>
+
+
+{/* another way to do this is to use classnames package */}
+import classNames from 'classnames';
+{/* after install and import classnames library, we define a class object*/}
+const btnClass = classNames('main-class' , {'if-true' : isActive});
