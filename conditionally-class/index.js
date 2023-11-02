6315ebd327ebb834c92d3aca140b313669e5a351
Adınız <erkanmurat0632@gmail.com>
2023-11-02 17:35:27 +0300

    another condition

--- conditionally-class/index.js
+++ conditionally-class/index.js
@@ -5,3 +5,9 @@ import React from 'react';
 isTrue = true;
 
 <div className={isTrue ? 'if-true-here' : 'if-false-here'  }>content</div>
+
+{/* if we have already a clasName and we want to add another class we do this */}
+
+<div className={`main-class ${condition}`}></div>
+{/* for example : */}
+<div className={`base-class ${isTrue ? 'if-true-here': 'if-false-here'}`}></div>
