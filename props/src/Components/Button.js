56abeda2e0ee36ddfd86f20c618f1ec4b4dea717
Adınız <erkanmurat0632@gmail.com>
2023-12-02 00:22:58 +0300

    props last commit

--- props/src/Components/Button.js
+++ props/src/Components/Button.js
@@ -4,4 +4,4 @@ export default function Button({ label, onClick }) {
   return <button onClick={onClick}>{label}</button>;
 }
 // butona props olarak fonksiyon gönderdim
-<Button label={"merhaba"} , onClick={()=> console.log('button clicked') }/>;
+<Button label={"merhaba"} onClick={() => console.log("button clicked")} />;
