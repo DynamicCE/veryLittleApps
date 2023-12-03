150f0b75661fcbde1e446168934c435529424ddc
Adınız <erkanmurat0632@gmail.com>
2023-12-04 00:43:35 +0300

    before states

--- mood-tracker/src/App.js
+++ mood-tracker/src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>there is somewhere will be a mooooood {"<3"}</p>
+    </div>
+  );
 }
 
 export default App;
