b7c06a087e93e943debdf590e40384472b538d30
Adınız <erkanmurat0632@gmail.com>
2023-12-05 02:08:25 +0300

    works

--- mood-tracker/src/App.js
+++ mood-tracker/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [Mood, setMood] = useState("MAIN MOOD IS NORMAL!");
+  const MoodSwitcherFunc = () => {
+    setMood("normal mode");
+  };
   return (
     <div className="App">
       <p>there is somewhere will be a mooooood {"<3"}</p>
+      <p>{Mood}</p>
+      <button onClick={MoodSwitcherFunc}>CLICK ME TO CHANGE THE MOOD</button>
     </div>
   );
 }
---
+++ mood-tracker/src/components/BadassMod.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function BadassMood() {
+  const [BadassMood, setBadassMod] = useState("BadASS");
+  return (
+    <div>
+      {" "}
+      <p></p>
+    </div>
+  );
+}
