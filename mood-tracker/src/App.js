3e78a4e0da2504fe81fb3cbe22c0e702d551ac9f
Adınız <erkanmurat0632@gmail.com>
2023-12-06 07:21:30 +0300

    bass mod

--- mood-tracker/src/App.js
+++ mood-tracker/src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import { useState } from "react";
-
+import BadassMood from "./components/BadassMod";
 function App() {
   const [Mood, setMood] = useState("MAIN MOOD IS NORMAL!");
   const MoodSwitcherFunc = () => {
     setMood("normal mode");
   };
+  const MoodSwitcherFunc2 = () => {
+    setMood(BadassMood);
+  };
   return (
     <div className="App">
       <p>there is somewhere will be a mooooood {"<3"}</p>
       <p>{Mood}</p>
-      <button onClick={MoodSwitcherFunc}>CLICK ME TO CHANGE THE MOOD</button>
+      <button onClick={MoodSwitcherFunc}>Click me to set normal mood!</button>
+
+      <BadassMood />
     </div>
   );
 }
--- mood-tracker/src/components/BadassMod.js
+++ mood-tracker/src/components/BadassMod.js
@@ -2,10 +2,13 @@ import { useState } from "react";
 
 export default function BadassMood() {
   const [BadassMood, setBadassMod] = useState("BadASS");
+  const MoodSwitcherBadass = () => {
+    setBadassMod("BadassMood!");
+  };
   return (
     <div>
-      {" "}
-      <p></p>
+      <p>{BadassMood}asdasd</p>
+      <button onClick={MoodSwitcherBadass}>Click me to set BADASS MOOD!</button>
     </div>
   );
 }
